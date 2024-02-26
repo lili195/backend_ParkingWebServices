b9d00576435fe9c04f1a4809c9f7a531b02c637a
lili_ana <lilianah33446@gmail.com>
2024-02-26 15:08:18 -0500

    funcion para el retiro de vehiculos

--- server.js
+++ server.js
@@ -38,13 +38,16 @@ app.post('/cars', upload.single('photo'), (req, res) => {
     }
 });
 
-
-
 app.get('/cars', (req, res) => {
     try {
-
         console.log('Solicitud GET recibida en /cars: ', new Date().toLocaleString());
-        const vehicles = vehiclesDB;
+        const vehicles = vehiclesDB.map(vehicle => ({
+            licensePlate: vehicle.licensePlate,
+            color: vehicle.color,
+            entryTime: vehicle.entryTime,
+            photo: getBase64Image(vehicle.photoPath)
+        }));
+
         console.log('Respondiendo con la lista de vehículos:', vehicles);
         res.status(200).json({ vehicles });
 
@@ -54,17 +57,28 @@ app.get('/cars', (req, res) => {
     }
 });
 
+// Función para convertir la imagen a base64
+function getBase64Image(path) {
+    if (!path) {
+        return null;
+    }
+
+    const fs = require('fs');
+    const image = fs.readFileSync(path);
+    return Buffer.from(image).toString('base64');
+}
+
 // // Middleware para retirar un carro por placa
 app.patch('/cars', (req, res) => {
     const retiredPlate = req.body;
     const retiredIndex = vehiclesDB.findIndex(vehicule => vehicule.licensePlate === retiredPlate);
-    if (retiredIndex != null){
+    if (retiredIndex != null) {
         vehiclesDB.splice(retiredIndex, 1);
         console.log("Vehiculo retirado exitosamente");
-        res.status(200).json({message: "Se retiro el vehiculo exitosamente"})
-    }else{
+        res.status(200).json({ message: "Se retiro el vehiculo exitosamente" })
+    } else {
         console.log("No se pudo retirar el vehiculo");
-        res.status(400).json({message: "No se retiro el vehiculo correctamente"})
+        res.status(400).json({ message: "No se retiro el vehiculo correctamente" })
     }
 });
 
