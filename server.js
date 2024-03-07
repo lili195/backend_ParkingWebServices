db26e08a2f1844b317f0364993261ccb9b26e686
VelaLaura30 <laura.vela03@uptc.edu.co>
2024-03-07 11:28:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const { upload } = require('./helpers/fileHandler');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
