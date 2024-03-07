ceb33f2a69663c3022aad63c4aeff7d1ac69f425
VelaLaura30 <laura.vela03@uptc.edu.co>
2024-03-07 13:19:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { upload } = require('./helpers/fileHandler');
 
 const app = express();
 const port = process.env.PORT
+const ipAddress = process.env.IP_ADDRESS
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -105,5 +106,5 @@ app.patch('/cars', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto 8000`)
+    console.log(`Servidor escuchando en ${ipAddress}:${port}`)
 })
