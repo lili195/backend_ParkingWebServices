887cf7a2359558ba74ac77bd851730aec27210d1
VelaLaura30 <laura.vela03@uptc.edu.co>
2024-03-17 12:51:23 -0500

    Conexión Base de Datos

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const cors = require('cors')
 const { upload } = require('./helpers/fileHandler');
+const { Pool } = require('pg');
 
 const app = express();
-const port = process.env.PORT
+
+const port = process.env.PORT;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -27,60 +29,71 @@ function logRequest(ip, method, url, message, body) {
     console.log(`([Ip: ${ip}, Fecha: ${date}, Hora: ${time}] , Solicitud: ${method}, Mensaje: ${message}, Respuesta: ${JSON.stringify(body)})`);
 }
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
+  });
+  
 
-
-// Endpoint para el registro de ingreso
-app.post('/cars', (req, res) => {
-
-    const originalBody = req.method === 'GET' ? req.query : req.body;
-    console.log('Original Body:', originalBody);
-
+  app.post('/cars', upload.single('photo'), async (req, res) => {
     logRequest(req.ip, 'POST', '/cars', 'Registro Carro', null);
+    if (!req.file) {
+        return res.status(400).json({ message: 'No se ha adjuntado ningún archivo' });
+    }
+
     const { licensePlate, color } = req.body;
     const entryTime = new Date();
     const photoPath = req.file ? req.file.path : null;
 
-    // Verificar si la placa ya existe en la base de datos
-    const existingIndex = vehiclesDB.findIndex(vehicle => vehicle.licensePlate === licensePlate);
-
-    if (existingIndex !== -1) {
-        logRequest(req.ip, 'POST', '/cars','La placa ya está registrada:', vehiclesDB[existingIndex] );
-        res.status(400).json({ message: 'La placa ya está registrada en el servidor' });
-    } else {
-        //if (photoPath) {
-            const vehicle = {
-                licensePlate,
-                color,
-                entryTime,
-                photoPath
-            };
-            vehiclesDB.push(vehicle);
-            logRequest(req.ip, 'POST', '/cars', 'Vehículo registrado con éxito:', vehicle);
-            res.status(200).json({ message: 'Entrada del vehículo registrada con éxito en el servidor', vehicle });
-        // } else {
-        //     logRequest(req.ip, 'POST', '/cars', 'No se pudo obtener la imagen:', photoPath);
-        //     res.status(401).json({ message: 'No fue posible registrar el vehículo en el servidor' });
-        // }
+    try {
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO vehicles (license_plate, color, entry_time, photo_path) VALUES ($1, $2, $3, $4)', [licensePlate, color, entryTime, photoPath]);
+        client.release();
+        logRequest(req.ip, 'POST', '/cars', 'Vehículo registrado con éxito:', { licensePlate, color, entryTime, photoPath });
+        res.status(200).json({ message: 'Entrada del vehículo registrada con éxito en la base de datos' });
+    } catch (err) {
+        console.error('Error al insertar vehículo en la base de datos:', err);
+        res.status(500).json({ message: 'Error interno del servidor al registrar el vehículo' });
     }
 });
 
-app.get('/cars', (req, res) => {
+app.get('/cars', async (req, res) => {
     try {
         logRequest(req.ip, 'GET', '/cars', 'Listar Carro');
-        const vehicles = vehiclesDB.map(vehicle => ({
-            licensePlate: vehicle.licensePlate,
-            color: vehicle.color,
-            entryTime: vehicle.entryTime,
-            photo: getBase64Image(vehicle.photoPath)
-        }));
-        logRequest(req.ip, 'GET', '/cars', 'Respondiendo con la lista de vehículos:', vehiclesDB);
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM vehicles');
+        const vehicles = result.rows;
+        client.release();
+        logRequest(req.ip, 'GET', '/cars', 'Respondiendo con la lista de vehículos:', vehicles);
         res.status(200).json({ vehicles });
-
     } catch (error) {
         console.error('Error al procesar la solicitud GET en /cars:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 });
+
+app.patch('/cars', async (req, res) => {
+    const { licensePlate } = req.body.licensePlate; 
+    logRequest(req.ip, 'PATCH', '/cars', 'Retirar Carro');
+    
+    try {
+        const client = await pool.connect();
+        const result = await client.query('DELETE FROM vehicles WHERE license_plate = $1', [licensePlate]);
+        client.release();
+        if (result.rowCount > 0) {
+            logRequest(req.ip, 'PATCH', '/cars', 'Vehículo retirado exitosamente');
+            res.status(200).json({ message: "Se retiró el vehículo exitosamente" });
+        } else {
+            logRequest(req.ip, 'PATCH', '/cars', 'No se pudo retirar el vehículo');
+            res.status(404).json({ message: "No se encontró el vehículo para retirar" });
+        }
+    } catch (err) {
+        console.error('Error al retirar vehículo de la base de datos:', err);
+        res.status(500).json({ message: 'Error interno del servidor al retirar el vehículo' });
+    }
+});
+
+
 
 // Función para convertir la imagen a base64
 function getBase64Image(path) {
@@ -93,25 +106,25 @@ function getBase64Image(path) {
     return Buffer.from(image).toString('base64');
 }
 
-// // Middleware para retirar un carro por placa
-app.patch('/cars', (req, res) => {
-    logRequest(req.ip, 'PATCH', '/cars', 'Retirar Carro');
-    const retiredPlate = req.body.licensePlate;
-    logRequest(req.ip, 'PATCH', '/cars', 'Placa Retirada: ' ,retiredPlate);
+// // // Middleware para retirar un carro por placa
+// app.patch('/cars', (req, res) => {
+//     logRequest(req.ip, 'PATCH', '/cars', 'Retirar Carro');
+//     const retiredPlate = req.body.licensePlate;
+//     logRequest(req.ip, 'PATCH', '/cars', 'Placa Retirada: ', retiredPlate);
 
-    const retiredIndex = vehiclesDB.findIndex(vehicle => vehicle.licensePlate === retiredPlate);
-    logRequest(req.ip, 'PATCH', '/cars', ' Posicion retirada: ',retiredIndex);
+//     const retiredIndex = vehiclesDB.findIndex(vehicle => vehicle.licensePlate === retiredPlate);
+//     logRequest(req.ip, 'PATCH', '/cars', ' Posicion retirada: ', retiredIndex);
 
 
-    if (retiredIndex !== -1) { // Cambiado para comparar con -1 en lugar de null
-        vehiclesDB.splice(retiredIndex, 1);
-        logRequest(req.ip, 'PATCH', '/cars', 'Vehiculo retirado exitosamente');
-        res.status(200).json({ message: "Se retiró el vehiculo exitosamente" });
-    } else {
-        logRequest(req.ip, 'PATCH', '/cars', 'No se pudo retirar el vehiculo');
-        res.status(400).json({ message: "No se retiró el vehiculo correctamente" });
-    }
-});
+//     if (retiredIndex !== -1) { // Cambiado para comparar con -1 en lugar de null
+//         vehiclesDB.splice(retiredIndex, 1);
+//         logRequest(req.ip, 'PATCH', '/cars', 'Vehiculo retirado exitosamente');
+//         res.status(200).json({ message: "Se retiró el vehiculo exitosamente" });
+//     } else {
+//         logRequest(req.ip, 'PATCH', '/cars', 'No se pudo retirar el vehiculo');
+//         res.status(400).json({ message: "No se retiró el vehiculo correctamente" });
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto: ${port}`)
